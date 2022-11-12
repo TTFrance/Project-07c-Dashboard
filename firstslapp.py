0ffb56613efb2d8390926c38e62daee719cb1b5a
TTFrance <105430056+TTFrance@users.noreply.github.com>
2022-11-12 15:52:20 +0100

    waterfall no height or width specified

--- firstslapp.py
+++ firstslapp.py
@@ -177,7 +177,7 @@ with tab1:
 
     # TEST REMOVAL OF THESE LINES TO SEE IF ERROR PERSISTS ON LIVE
 
-    st_shap(shap.plots.waterfall(shap_values[pos_num], max_display=9), height=300)
+    st_shap(shap.plots.waterfall(shap_values[pos_num], max_display=9))
 
     #fig = shap.plots.waterfall(shap_values[pos_num], max_display=9)
     #st.pyplot(fig, matplotlib=True)
