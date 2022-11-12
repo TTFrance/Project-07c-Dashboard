3d5f8a50d53cad5f4b93467d891917cb7796d43b
TTFrance <105430056+TTFrance@users.noreply.github.com>
2022-11-12 15:12:25 +0100

    yyy

--- firstslapp.py
+++ firstslapp.py
@@ -177,7 +177,7 @@ with tab1:
 
     # TEST REMOVAL OF THESE LINES TO SEE IF ERROR PERSISTS ON LIVE
 
-    st_shap(shap.plots.waterfall(shap_values[pos_num], max_display=9))
+    st_shap(shap.plots.waterfall(shap_values[pos_num], max_display=9),width=500)
 
     #fig = shap.plots.waterfall(shap_values[pos_num], max_display=9)
     #st.pyplot(fig, matplotlib=True)
