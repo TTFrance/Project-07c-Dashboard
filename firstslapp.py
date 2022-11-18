15bff68238a90d38c082cd5488b3dc10b6554fb5
TTFrance <105430056+TTFrance@users.noreply.github.com>
2022-11-18 12:05:27 +0100

    added a lot of new functionality

--- firstslapp.py
+++ firstslapp.py
@@ -50,6 +50,7 @@ df = pd.read_pickle('data/client_data_api_dashboard_1k.pkl')
 
 # LOAD CURVES FOR PROFIT / LOSS
 CURVES = pd.read_pickle('data/curves.pkl')
+CURVES2 = pd.read_pickle('data/curves_new.pkl')
 
 # ANNUITY AMOUNTS
 df_amt_annuity = pd.read_pickle('data/df_amt_annuity.pkl')
@@ -61,7 +62,7 @@ dictionary = {'A: Low': ['226060', '310013'], 'B: Medium': ['324973', '449031'],
 sidebar = st.sidebar
 
 # CREATE MAIN TABS
-tab1, tab2, tab3 = st.tabs(["Explanations", "Main Features", "Secondary Features"])
+tab1, tab2, tab3 = st.tabs(["Explanations", "Feature Scatterplots", "Main Features"])
 
 # functions
 def plot_shap_summary_feature(shap_values,X,feature_name):
@@ -151,14 +152,8 @@ with sidebar:
     # END SIDEBAR
 
 
-# TAB 1 - MAIN DETAILS
+# TAB 1 - EXPLANATIONS
 with tab1:
-    # tab1_left_column, tab1_right_column = st.columns([1, 2], gap="small")
-    # with tab1_left_column:
-    # END TAB1 LEFT COLUMN
-    # with tab1_right_column:
-    # END TAB1 RIGHT COLUMN
-
     #features = pd.DataFrame(columns=['name','importance'])
     #importance = model.feature_importances_
     #for i,v in enumerate(importance):
@@ -169,128 +164,168 @@ with tab1:
     #ax.barh(data=features.sort_values('importance'),y='name',width='importance')
     #st.pyplot(fig, matplotlib=True)
 
+    tab1_1, tab1_2, tab1_3 = st.tabs(["Local Explanation", "Global Feature Importance", "Risk Thresholds"])
+
     # SHAP WATERFALL
-    st.subheader('Local Explanation')
-    X = df.drop(columns=['TARGET'])
-    df2 = df.index.to_frame().reset_index(drop=True)
-    pos_num = df2[df2['SK_ID_CURR'] == int(client_id)].index[0]
-    explainer = shap.Explainer(model, X)
-    shap_values = explainer(X)
-    st.set_option('deprecation.showPyplotGlobalUse', False)
+    with tab1_1:
+        st.subheader('Local Explanation')
+        X = df.drop(columns=['TARGET'])
+        df2 = df.index.to_frame().reset_index(drop=True)
+        pos_num = df2[df2['SK_ID_CURR'] == int(client_id)].index[0]
+        explainer = shap.Explainer(model, X)
+        shap_values = explainer(X)
+        st.set_option('deprecation.showPyplotGlobalUse', False)
 
-    fig = shap.plots.waterfall(shap_values[pos_num], max_display=9)
-    st.pyplot(fig, matplotlib=True)
+        fig = shap.plots.waterfall(shap_values[pos_num], max_display=9)
+        st.pyplot(fig, matplotlib=True)
 
-    st.success('**The Local Explanation:** \
-        \n\nThis shows the effect that each feature has on a single prediction in the model. \
-        \n\nThe bottom of a waterfall plot starts as the expected value of the model output,  \
-        and then each row shows how the positive (red) or negative (blue) contribution of each  \
-        feature moves the value from the expected model output over the background dataset \
-        to the model output for this prediction. \
-        \n\nA blue arrow pointing to the left moves the prediction towards being less likely of default, \
-        and a red arrow pointing to the right moves the prediction towards being more likely')
-
-
-    # -----------------
-    # SHAP
-    # DO NOT TOUCH THIS
-    # DO NOT TOUCH THIS
-    # DO NOT TOUCH THIS
-    X = df.drop(columns=['TARGET'])
-    explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(X)
-    # LOCAL EXPLANATION
-    # STREAMLIT_SHAP
-    #st.subheader('Local Explanation')
-    #st_shap.st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1][0,:], X.iloc[0,:]))
-    # GLOBAL EXPLANATION
-    st.subheader('Global Feature Importance')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    #fig = shap.summary_plot(shap_values[1], X, show = False, cmap=plt.get_cmap("viridis_r"))
-    fig = shap.summary_plot(shap_values[1], X, show = False, plot_type='violin')
-    st.pyplot(fig, matplotlib=True)
-    # DO NOT TOUCH THIS
-    # DO NOT TOUCH THIS
-    # DO NOT TOUCH THIS
-    # -----------------
-    st.success('**To interpret a SHAP model explainer**: \n\n Higher feature values are in red, \
-        lower are in blue. Taking EXT_SOURCE_2 as an example we can see that higher values \
-        result in a bigger negative effect on the model prediction (probability of default). \
-        \n\n Therefore clients with higher EXT_SOURCE_2 values are less likely to default.')
+        st.success('**The Local Explanation:** \
+            \n\nThis shows the effect that each feature has on a single prediction in the model. \
+            \n\nThe bottom of a waterfall plot starts as the expected value of the model output,  \
+            and then each row shows how the positive (red) or negative (blue) contribution of each  \
+            feature moves the value from the expected model output over the background dataset \
+            to the model output for this prediction. \
+            \n\nA blue arrow pointing to the left moves the prediction towards being less likely of default, \
+            and a red arrow pointing to the right moves the prediction towards being more likely')
 
 
+    with tab1_2:
+        st.subheader('Global Feature Importance')
+        # -----------------
+        # SHAP
+        # DO NOT TOUCH THIS
+        # DO NOT TOUCH THIS
+        # DO NOT TOUCH THIS
+        X = df.drop(columns=['TARGET'])
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(X)
+        # LOCAL EXPLANATION
+        # STREAMLIT_SHAP
+        #st.subheader('Local Explanation')
+        #st_shap.st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1][0,:], X.iloc[0,:]))
+        # GLOBAL EXPLANATION
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        #fig = shap.summary_plot(shap_values[1], X, show = False, cmap=plt.get_cmap("viridis_r"))
+        fig = shap.summary_plot(shap_values[1], X, show = False, plot_type='violin')
+        st.pyplot(fig, matplotlib=True)
+        # DO NOT TOUCH THIS
+        # DO NOT TOUCH THIS
+        # DO NOT TOUCH THIS
+        # -----------------
+        st.success('**To interpret a SHAP model explainer**: \n\n Higher feature values are in red, \
+            lower are in blue. Taking EXT_SOURCE_2 as an example we can see that higher values \
+            result in a bigger negative effect on the model prediction (probability of default). \
+            \n\n Therefore clients with higher EXT_SOURCE_2 values are less likely to default.')
 
-    # GAIN / LOSS CURVES GRAPH
-    st.subheader('Gain / Loss Curves')
-    fig, ax = plt.subplots(figsize =(10, 6))
-    CURVES.plot(ax=ax, x='threshold', y=['EARNED', 'LOST'])
-    plt.axvline(x=default_proba, color='red', linestyle='--', linewidth=2, alpha=0.5)
-    st.pyplot(fig)
+    with tab1_3:
+        st.subheader('Risk Thresholds')
+        # GAIN / LOSS CURVES GRAPH
+        #st.subheader('Gain / Loss Curves')
+        #fig, ax = plt.subplots(figsize =(10, 6))
+        #CURVES.plot(ax=ax, x='threshold', y=['EARNED', 'LOST'])
+        #plt.axvline(x=default_proba, color='red', linestyle='--', linewidth=2, alpha=0.5)
+        #st.pyplot(fig)
+
+        # CURVES NEW
+        risk = st.selectbox(
+            'Select your acceptable level of risk:',
+            ('Low', 'Medium', 'High')
+        )
+
+        risk_medium_lower = 0.360
+        risk_medium_upper = 0.565
+        risk_band = 0.2
+
+        risk_graph_title = 'Client placement within risk thresholds'
+
+        if risk == 'Medium':
+            risk_lower = risk_medium_lower
+            risk_upper = risk_medium_upper
+        elif risk == 'Low':
+            risk_lower = risk_medium_lower - risk_band
+            risk_upper = risk_medium_upper - risk_band
+        else: # risk == high:
+            risk_lower = risk_medium_lower + risk_band
+            risk_upper = risk_medium_upper + risk_band
+
+        COLORS = dict({'CONSTANT_COST':'blue', 'ACCEPTING_COST':'red', 'REJECTING_COST':'orange'})
+        COMMON_GRAPH_OPTIONS = dict(x='threshold',color=COLORS)
+        fig, ax = plt.subplots(figsize =(10, 6))
+        CURVES2.plot.area(
+            ax=ax,
+            y=['CONSTANT_COST', 'REJECTING_COST', 'ACCEPTING_COST', ],
+            stacked=True,
+            **COMMON_GRAPH_OPTIONS
+        )
+        W0 =  27108  # penalty=12.5
+        W1 =  599026  #
+        ax.plot([0, 1],[-W0, -W0], ':')
+        ax.set_xlim(0,1)
+        ax.legend(loc='lower left')
+
+        plt.axvline(x=default_proba, ymin=0, ymax=1, color='black', linestyle='--', linewidth=1, alpha=0.4)
+        plt.axvline(x=risk_lower, ymin=0, ymax=1, color='green', linestyle='--', linewidth=1, alpha=0.4)
+        plt.axvline(x=risk_upper, ymin=0, ymax=1, color='darkred', linestyle='--', linewidth=1, alpha=0.4)
+
+        ax.annotate('low risk threshold',
+                    xy=(risk_lower, -40000), xycoords='data', color='green',
+                    xytext=(-15, 25), textcoords='offset points',
+                    arrowprops=dict(facecolor='green', shrink=0.05),
+                    horizontalalignment='right', verticalalignment='bottom')
+
+        ax.annotate('high risk threshold',
+                    xy=(risk_upper, -40000), xycoords='data', color='darkred',
+                    xytext=(15, 25), textcoords='offset points',
+                    arrowprops=dict(facecolor='darkred', shrink=0.05),
+                    horizontalalignment='left', verticalalignment='bottom')
+
+        ax.annotate('client probability of default',
+                    xy=(default_proba, -30000), xycoords='data', color='black',
+                    xytext=(-15, 25), textcoords='offset points',
+                    arrowprops=dict(facecolor='black', shrink=0.05),
+                    horizontalalignment='right', verticalalignment='bottom')
+
+        plt.suptitle(risk_graph_title)
+        st.pyplot(fig)
+
+        # in which category is the client regarding the currently selected risk?
+        if default_proba <= risk_lower:
+            risk_client = 'Low'
+        elif default_proba >= risk_upper:
+            risk_client = 'High'
+        else:
+            risk_client = 'Medium'
+
+        base_text = 'The green line is low risk threshold. \
+                    Above the red line is high risk threshold. \
+                    Inbetween the green and red lines is medium risk band. \
+                    \n\nThe black line is the clients calculated probability of default.\n\n'
+
+        if risk_client == 'Medium':
+            extra_text = 'This client falls into the band of **medium** risk and you should \
+                study the clients values for the most important features as shown in \
+                study the clients values for the most important features as shown in \
+                the 2nd and 3rd tabs, to draw a manual conclusion on whether or not to agree the loan.'
+        elif risk_client == 'Low':
+            extra_text = 'This client falls into the band of **low** risk and could be \
+                **accepted** with no further manual analysis, however please take a moment to \
+                study the clients values for the most important features as shown in \
+                the 2nd and 3rd tabs, to make sure you agree with this recommendation.'
+        else: # risk == high:
+            extra_text = 'This client falls into the band of **high** risk and could be \
+                rejected with no further manual analysis, however please take a moment to \
+                study the clients values for the most important features as shown in \
+                the 2nd and 3rd tabs, to make sure you agree with this recommendation.'
+
+        risk_explanation_text = base_text + extra_text
+
+        st.success(risk_explanation_text)
 
 
 
 # TAB 2 - 4 HISTOGRAMS TOP 4 FEATURES
 with tab2:
-    tab2_left_column, tab2_right_column = st.columns([1, 1], gap="small")
-    with tab2_left_column:
-
-        # Group data together
-        #EXT_SOURCE_2_data = df.EXT_SOURCE_2.values
-        #EXT_SOURCE_2_data2 = []
-        #EXT_SOURCE_2_data2.append(EXT_SOURCE_2_data)
-        #group_labels = ['EXT_SOURCE_2']
-        # Create distplot with custom bin_size
-        #fig = ff.create_distplot(EXT_SOURCE_2_data2, group_labels, bin_size=[.05])
-        # Plot!
-        #st.plotly_chart(fig, use_container_width=True)
-
-
-        st.subheader('EXT_SOURCE_2')
-        st.markdown('Value: <span style="color:red">**' + str(round(EXT_SOURCE_2,4)) + '**</span>', unsafe_allow_html=True)
-        fig = plt.figure(figsize=(6, 4))
-        sns.kdeplot(data=df, x="EXT_SOURCE_2", fill=True, color='blue').set(title='Dist EXT_SOURCE_2')
-        plt.axvline(x=EXT_SOURCE_2, color='red', linestyle='--', linewidth=2, alpha=0.5)
-        st.pyplot(fig)
-        #plot_shap_summary_feature(shap_values[1],X,'EXT_SOURCE_2')
-        #pos = get_feature_index('EXT_SOURCE_2',X)
-        #fig = shap.summary_plot(shap_values[1][:,pos:pos+1], X.iloc[:, pos:pos+1])
-        #st.pyplot(fig, matplotlib=True)
-
-
-        st.subheader('EXT_SOURCE_3')
-        st.markdown('Value: <span style="color:red">**' + str(round(EXT_SOURCE_3,4)) + '**</span>', unsafe_allow_html=True)
-        fig = plt.figure(figsize=(6, 4))
-        sns.kdeplot(data=df, x="EXT_SOURCE_3", fill=True, color='orange').set(title='Dist EXT_SOURCE_3')
-        plt.axvline(x=EXT_SOURCE_3, color='red', linestyle='--', linewidth=2, alpha=0.5)
-        st.pyplot(fig)
-        #plot_shap_summary_feature(shap_values[1],X,'EXT_SOURCE_3')
-
-    # END TAB2 LEFT COLUMN
-
-    # RIGHT COLUMN
-    with tab2_right_column:
-
-        st.subheader('PAYMENT_RATE')
-        st.markdown('Value: <span style="color:red">**' + str(round(PAYMENT_RATE,4)) + '**</span>', unsafe_allow_html=True)
-        fig = plt.figure(figsize=(6, 4))
-        sns.kdeplot(data=df, x="PAYMENT_RATE", fill=True, color='green').set(title='Dist PAYMENT_RATE')
-        plt.axvline(x=PAYMENT_RATE, color='red', linestyle='--', linewidth=2, alpha=0.5)
-        st.pyplot(fig)
-        #plot_shap_summary_feature(shap_values[1],X,'PAYMENT_RATE')
-
-        st.subheader('AMT_CREDIT')
-        st.markdown('Value: <span style="color:red">**' + str(round(AMT_CREDIT,4)) + '**</span>', unsafe_allow_html=True)
-        fig = plt.figure(figsize=(6, 4))
-        sns.kdeplot(data=df, x="AMT_CREDIT", fill=True, color='yellow').set(title='Dist AMT_CREDIT')
-        plt.axvline(x=AMT_CREDIT, color='red', linestyle='--', linewidth=2, alpha=0.5)
-        st.pyplot(fig)
-        #plot_shap_summary_feature(shap_values[1],X,'AMT_CREDIT')
-    # END TAB2 RIGHT COLUMN
-
-# TAB 3 - 4 HISTOGRAMS NEXT 4 FEATURES
-with tab3:
     st.subheader('Feature Comparison - Actual Values')
-    tab3_left_column, tab3_right_column = st.columns([1, 1], gap="small")
     df_shap_values = pd.DataFrame(data=shap_values[1],columns=df.drop(columns=['TARGET']).columns)
     df_feature_importance = pd.DataFrame(columns=['feature','importance'])
     for col in df_shap_values.columns:
@@ -298,9 +333,10 @@ with tab3:
         df_feature_importance.loc[len(df_feature_importance)] = [col,importance]
     df_feature_importance = df_feature_importance.sort_values('importance',ascending=False)
     top_10_features = df_feature_importance.feature.head(10).to_list()
-    with tab3_left_column:
+    tab2_left_column, tab2_right_column = st.columns([1, 1], gap="small")
+    with tab2_left_column:
         featureX = st.selectbox('Select X Feature:', top_10_features)
-    with tab3_right_column:
+    with tab2_right_column:
         featureY = st.selectbox('Select Y Feature:', top_10_features)
 
     if featureX == featureY:
@@ -356,4 +392,63 @@ with tab3:
     fig.suptitle('SHAP Feature Values, Client ' + client_id)
     st.pyplot(fig)
 
+# TAB 3 - 4 HISTOGRAMS 4 FEATURES
+with tab3:
+    tab3_left_column, tab3_right_column = st.columns([1, 1], gap="small")
+    with tab3_left_column:
 
+        # Group data together
+        #EXT_SOURCE_2_data = df.EXT_SOURCE_2.values
+        #EXT_SOURCE_2_data2 = []
+        #EXT_SOURCE_2_data2.append(EXT_SOURCE_2_data)
+        #group_labels = ['EXT_SOURCE_2']
+        # Create distplot with custom bin_size
+        #fig = ff.create_distplot(EXT_SOURCE_2_data2, group_labels, bin_size=[.05])
+        # Plot!
+        #st.plotly_chart(fig, use_container_width=True)
+
+
+        st.subheader('EXT_SOURCE_2')
+        st.markdown('Value: <span style="color:red">**' + str(round(EXT_SOURCE_2,4)) + '**</span>', unsafe_allow_html=True)
+        fig = plt.figure(figsize=(6, 4))
+        sns.kdeplot(data=df, x="EXT_SOURCE_2", fill=True, color='blue').set(title='Dist EXT_SOURCE_2')
+        plt.axvline(x=EXT_SOURCE_2, color='red', linestyle='--', linewidth=2, alpha=0.5)
+        st.pyplot(fig)
+        #plot_shap_summary_feature(shap_values[1],X,'EXT_SOURCE_2')
+        #pos = get_feature_index('EXT_SOURCE_2',X)
+        #fig = shap.summary_plot(shap_values[1][:,pos:pos+1], X.iloc[:, pos:pos+1])
+        #st.pyplot(fig, matplotlib=True)
+
+
+        st.subheader('EXT_SOURCE_3')
+        st.markdown('Value: <span style="color:red">**' + str(round(EXT_SOURCE_3,4)) + '**</span>', unsafe_allow_html=True)
+        fig = plt.figure(figsize=(6, 4))
+        sns.kdeplot(data=df, x="EXT_SOURCE_3", fill=True, color='orange').set(title='Dist EXT_SOURCE_3')
+        plt.axvline(x=EXT_SOURCE_3, color='red', linestyle='--', linewidth=2, alpha=0.5)
+        st.pyplot(fig)
+        #plot_shap_summary_feature(shap_values[1],X,'EXT_SOURCE_3')
+
+    # END TAB2 LEFT COLUMN
+
+    # RIGHT COLUMN
+    with tab3_right_column:
+
+        st.subheader('PAYMENT_RATE')
+        st.markdown('Value: <span style="color:red">**' + str(round(PAYMENT_RATE,4)) + '**</span>', unsafe_allow_html=True)
+        fig = plt.figure(figsize=(6, 4))
+        sns.kdeplot(data=df, x="PAYMENT_RATE", fill=True, color='green').set(title='Dist PAYMENT_RATE')
+        plt.axvline(x=PAYMENT_RATE, color='red', linestyle='--', linewidth=2, alpha=0.5)
+        st.pyplot(fig)
+        #plot_shap_summary_feature(shap_values[1],X,'PAYMENT_RATE')
+
+        st.subheader('AMT_CREDIT')
+        st.markdown('Value: <span style="color:red">**' + str(round(AMT_CREDIT,4)) + '**</span>', unsafe_allow_html=True)
+        fig = plt.figure(figsize=(6, 4))
+        sns.kdeplot(data=df, x="AMT_CREDIT", fill=True, color='yellow').set(title='Dist AMT_CREDIT')
+        plt.axvline(x=AMT_CREDIT, color='red', linestyle='--', linewidth=2, alpha=0.5)
+        st.pyplot(fig)
+        #plot_shap_summary_feature(shap_values[1],X,'AMT_CREDIT')
+    # END TAB2 RIGHT COLUMN
+
+
+        
